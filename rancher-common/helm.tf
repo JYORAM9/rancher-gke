357671daa9f653eac280fdf02947769cfc56121b
JYORAM9 <33308230+JYORAM9@users.noreply.github.com>
2023-10-27 00:21:38 -0400

    Update helm.tf

--- rancher-common/helm.tf
+++ rancher-common/helm.tf
@@ -1,24 +1,24 @@
 # Helm resources
 
 # Install cert-manager helm chart
-resource "helm_release" "cert_manager" {
-  name             = "cert-manager"
-  chart            = "https://charts.jetstack.io/charts/cert-manager-v${var.cert_manager_version}.tgz"
-  namespace        = "cert-manager"
-  create_namespace = true
-  wait             = true
+//resource "helm_release" "cert_manager" {
+ // name             = "cert-manager"
+ // chart            = "https://charts.jetstack.io/charts/cert-manager-v${var.cert_manager_version}.tgz"
+ // namespace        = "cert-manager"
+ // create_namespace = true
+ // wait             = true
 
-  set {
-    name  = "installCRDs"
-    value = "true"
-  }
-}
+  //set {
+   // name  = "installCRDs"
+    //value = "true"
+  //}
+//}
 
 # Install Rancher helm chart
 resource "helm_release" "rancher_server" {
-  depends_on = [
-    helm_release.cert_manager,
-  ]
+  //depends_on = [
+    //helm_release.cert_manager,
+  //]
 
   name             = "rancher"
   chart            = "${var.rancher_helm_repository}/rancher-${var.rancher_version}.tgz"
